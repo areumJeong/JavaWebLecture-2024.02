292f7f0289f1c0846d64c392a1a8253addcbb13f
areumJeong <add5833@naver.com>
2024-02-16 17:29:08 +0900

    DB Access

--- src/main/java/ch07_dao/kcity/KcityController.java
+++ src/main/java/ch07_dao/kcity/KcityController.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Method;
 import java.util.List;
 
 import ch07_dao.City;
@@ -17,7 +16,7 @@ import ch07_dao.CityDao;
 /**
  * Servlet implementation class KcityController
  */
-@WebServlet({"/ch07/kcity/list", "/ch07/kcity/insert", "/ch07/kcity/update",
+@WebServlet({"/ch07/kcity/list", "/ch07/kcity/insert", "/ch07/kcity/update", 
 			"/ch07/kcity/delete", "/ch07/kcity/wrong"})
 public class KcityController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -29,7 +28,7 @@ public class KcityController extends HttpServlet {
 		String action = uri[uri.length - 1];
 		String method = request.getMethod();
 		RequestDispatcher rd = null;
-		String name = null, countryCode = null, district =null, population_ = null;
+		String name = null, countryCode = null, district = null, population_ = null;
 		int id = 0, population = 0;
 		City city = null;
 		String[] districts = "Cheju,Chollabuk,Chollanam,Chungchongbuk,Chungchongnam,Inchon,Kang-won,Kwangju,Kyonggi,Kyongsangbuk,Kyongsangnam,Pusan,Seoul,Taegu,Taejon".split(",");
@@ -44,14 +43,15 @@ public class KcityController extends HttpServlet {
 			int offset = (offset_ == null || offset_.equals("")) ? 0 : Integer.parseInt(offset_);
 			
 			List<City> list = cDao.getCityList(district, num, offset);
-			rd = request.getRequestDispatcher("/ch07/kcity/list2.jsp");
+			rd = request.getRequestDispatcher("/ch07/kcity/list.jsp");
 			request.setAttribute("list", list);
 			rd.forward(request, response);
 			break;
 			
 		case "insert":
 			if (method.equals("GET")) {
-				rd=request.getRequestDispatcher("/ch07/kcity/insert2.jsp");
+				rd = request.getRequestDispatcher("/ch07/kcity/insert.jsp");
+				request.setAttribute("districts", districts);
 				rd.forward(request, response);
 			} else {
 				name = request.getParameter("name");
@@ -66,24 +66,25 @@ public class KcityController extends HttpServlet {
 				response.sendRedirect("/jw/ch07/kcity/list?district=" + district + "&num=30&offset=0");
 			}
 			break;
+			
 		case "update":
 			if (method.equals("GET")) {
-				id = Integer.parseInt(request.getParameter("id"));				
+				id = Integer.parseInt(request.getParameter("id"));
 				city = cDao.getCity(id);
-				rd = request.getRequestDispatcher("/ch07/kcity/update2.jsp");
+				rd = request.getRequestDispatcher("/ch07/kcity/update.jsp");
 				request.setAttribute("city", city);
 				request.setAttribute("districts", districts);
 				rd.forward(request, response);
 			} else {
-				 id = Integer.parseInt(request.getParameter("id"));
-				 name = request.getParameter("name");
-				 countryCode = request.getParameter("countryCode");
-				 district = request.getParameter("district");
-				 population_ = request.getParameter("population");
-				 population = (population_.equals("")) ? 0 : Integer.parseInt(population_);
-				 city = new City(id, name, countryCode, district, population);
-				 cDao.updateCity(city);
-				 response.sendRedirect("/jw/ch07/kcity/list?district=" + district + "&num=30&offset=0");
+				id = Integer.parseInt(request.getParameter("id"));
+				name = request.getParameter("name");
+				countryCode = request.getParameter("countryCode");
+				district = request.getParameter("district");
+				population_ = request.getParameter("population");
+				population = (population_.equals("")) ? 0 : Integer.parseInt(population_);
+				city = new City(id, name, countryCode, district, population);
+				cDao.updateCity(city);
+				response.sendRedirect("/jw/ch07/kcity/list?district=" + district + "&num=30&offset=0");
 			}
 			break;
 			
@@ -94,7 +95,7 @@ public class KcityController extends HttpServlet {
 			break;
 			
 		default:
-			rd = request.getRequestDispatcher("/ch07/kcity/alertMsg2.jsp");
+			rd = request.getRequestDispatcher("/ch07/kcity/alertMsg.jsp");
 			request.setAttribute("msg", "잘못된 접근입니다.");
 			request.setAttribute("url", "/jw/ch07/kcity/list");
 			rd.forward(request, response);
