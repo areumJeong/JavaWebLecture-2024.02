3d1c5cb727d8f3765d05d9f6d9cdc84288a98eab
areumJeong <add5833@naver.com>
2024-02-19 11:47:17 +0900

    session

--- src/main/java/ch09_cookie_session/Ex02_GetSession.java
+++ src/main/java/ch09_cookie_session/Ex02_GetSession.java
@@ -9,28 +9,31 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
+import org.apache.catalina.tribes.util.Arrays;
 
 @WebServlet("/ch09/getSession")
 public class Ex02_GetSession extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		
 		int price = (Integer) session.getAttribute("price");
 		String uid = (String) session.getAttribute("uid");
-		System.out.println("price: " + price + ", uid: " + uid);
 		String[] fruits = (String[]) session.getAttribute("fruits");
 		System.out.println(Arrays.toString(fruits));
 		
 		String sessionId = session.getId();
+		int interval = session.getMaxInactiveInterval();
 		
 		PrintWriter out = response.getWriter();
 		response.setContentType("text/html; charset=utf-8");
-		out.print("<h1>price: \" + price + \", uid: \" + uid + "</h1>");
-		out.print("<h1>" + Arrays.toString(fruits) + "</h1></br>");
+		out.print("<h1>price: " + price + ", uid: " + uid + "</h1>");
+		out.print("<h1>" + Arrays.toString(fruits) + "</h1><br>");
+		out.print("<h1>isNew: " + session.isNew());
 		out.print("<h1>id: " + sessionId);
-	
+		out.print("<h1>maxInactiveInterval: " + interval + " 초");
+	}
+
+}
